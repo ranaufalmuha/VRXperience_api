de1f4a30b6cfb65b6d4a3df34838dcac753015fb
ranaufalmuha <ifal.muha@gmail.com>
2024-09-07 10:20:19 +0700

    update landing page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        "./public/**/*.{js,jsx,ts,tsx,html}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "accent": "green",
+            },
+        },
+    },
+    plugins: [],
+}
